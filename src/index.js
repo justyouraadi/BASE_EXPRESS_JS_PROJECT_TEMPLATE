182f1f61a02e04daf4af483a2fc1c055e794ca9c
justyouraadi <131365821+justyouraadi@users.noreply.github.com>
2024-01-20 17:48:32 +0530

    latest code

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const {PORT} = require("./config")
+
+app.get('/',(req,res)=>{
+})
+
+app.listen(PORT,()=>{
+    console.log(`successfully started the server on PORT : ${PORT}`);
+});
